e70fcc1f379af71ec815d8f82325536ca8b74c45
jessiBramwell <bramibly@gmail.com>
2019-08-26 12:08:56 -0600

    removed color change and nav-box resize feature | polished responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,95 @@
-$(window).scroll(function () {
-  // selectors
-  var $window = $(window),
-    $panel = $('section'),
-    $box = $('#color-block');
 
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 3);
+(function ($) {
 
-  $panel.each(function () {
-    var $this = $(this);
-      
+  $(document).on("scroll", onScroll);
 
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
-      // Remove all classes on body with color-
-      $box.removeClass(function (index, css) {
-        return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
-      });
+  function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.scroll').each(function () {
 
-      // Add class of currently active div
-      $box.addClass('color-' + $(this).data('color'));
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+
+      if (scrollPos < 300) {
+        $('.scroll:first-child').addClass("active");
+      }
+      else if (refElement.position().top <= (scrollPos + 300) && refElement.position().top + refElement.height() > (scrollPos + 200)) {
+        $('a').removeClass("active");
+        currLink.addClass("active");
+      }
+      else {
+        currLink.removeClass("active");
+      }
+    });
+  }
+
+
+  const $window = $(window);
+  const $header = $('.skills-list h4');
+  const $ul = $('.skills-list_ul');
+  const $contact = $('#contact');
+
+  $ul.on('show.bs.collapse', toggleChevronList);
+  $ul.on('hide.bs.collapse', toggleChevronList);
+  $('.contact').on('show.bs.collapse', toggleChevronNav);
+  $('.contact').on('hide.bs.collapse', toggleChevronNav);
+
+  function resize() {
+    if ($window.width() < 769) {
+      $contact.addClass('show')
+    }
+    else {
+      $contact.removeClass('show')
     }
 
-  });
+    if ($window.width() < 992) {
+      $ul.addClass('collapse');
+      $header.attr('data-toggle', 'collapse');
+      checkShow();
+    }
+    else {
+      $header.removeAttr('data-toggle');
+      $ul.removeClass('collapse hide');
+    }
+  }
 
-}).scroll();
+  function toggleChevronList(e) {
+    $(e.target)
+      .siblings('h4')
+      .children('.chevron')
+      .children('.fas')
+      .toggleClass('fa-chevron-right fa-chevron-down');
+  }
+
+  function toggleChevronNav(e) {
+    console.log(e.target);
+
+    $('.icon-title')
+      .children('i')
+      .toggleClass('fa-chevron-right fa-chevron-down');
+  }
+
+  function checkShow() {
+    if ($ul.hasClass('show')) {
+      $ul.each(function () {
+        if ($(this).hasClass('show')) {
+
+          $(this)
+            .siblings('h4')
+            .children('.chevron')
+            .children('.fas')
+            .removeClass('fa-chevron-right')
+            .addClass('fa-chevron-down');
+        }
+      })
+    }
+  }
+
+
+  $window
+    .resize(resize)
+    .trigger('resize');
+})(jQuery);
+
+
 
